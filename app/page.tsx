9c9b1171058abb716437d21ba87c891859f04154
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 04:58:01 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+//GRID
+
+const Page = () => {
+  <div className="container mx-auto">
+    <div className="p-3 rounded-md bg-gray-400">
+      <div className="grid grid-cols-1 gap-2">
+        <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-001.jpg)]"></div>
+        <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-003.jpg)]"></div>
+        <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-006.jpg)]"></div>
+      </div>
+
+    </div>
+
+  </div>
+};
+
+export default Page;
