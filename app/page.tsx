fc7adb7dad6ede7b180ac9564b048794eb7e2c9c
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 05:18:55 -0300

    1 file modified

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ const Page = () => {
   return(
   <div className="container mx-auto">
     <div className="p-3 rounded-md bg-gray-400">
-      <div className="grid grid-cols-1 gap-2">
-        <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-001.jpg)]"></div>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-3 ">
+        <div className="w-full h-60 bg-cover bg-center md:col-span-2 md:row-span-2 md:h-full bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-001.jpg)]"></div>
         <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-003.jpg)]"></div>
         <div className="w-full h-60 bg-cover bg-center bg-[url(https://amazingarchitecture.com/storage/files/1/Architecture%20firms/Kireeva%20Anna/Black%20house%20in%20the%20woods/Black-house-in-the-woods-Kireeva-Anna-USA-006.jpg)]"></div>
       </div>
